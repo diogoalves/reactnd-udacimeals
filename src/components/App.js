31d7aa64b8662aea07bb5d2d3ae1424367d7db6d
casa <casa@casas-iMac.lan>
2018-06-13 08:18:00 -0300

    fix security problems in old dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -52,7 +52,6 @@ class App extends Component {
 
   openIngredientsModal = () => { 
     this.setState(() => ({ ingredientsModalOpen: true })) 
-    console.log("entrou")
   }
 
   closeIngredientsModal = () => this.setState(() => ({ ingredientsModalOpen: false }))
